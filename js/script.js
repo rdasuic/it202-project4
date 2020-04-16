7b612fa4362fc1c6df3607c956af4e0050cde683
Rakesh Das <rdas7@uic.edu>
2020-04-16 23:49:22 +0000

    Added selective stats to plot for each country

--- js/script.js
+++ js/script.js
@@ -88,7 +88,7 @@ viewChartBtnEl.addEventListener('click', () => {
 });
 
 addCountryBtnEl.addEventListener('click', () =>{
-    if(countryInputEl.value == '' || (!confirmedCheckbox.checked && !deathsCheckbox.checked && !recoveredCheckbox.checked)) {
+    if(countryInputEl.value == '') {
         errorDialog.open();
     }
     else {
@@ -139,30 +139,138 @@ const showCountriesDataTable = () => {
 const addCountryToList = () => {
     const selectedCountriesListTableBody = document.querySelector('#country-list-content');
     const selectedCountry = countryInputEl.value;
+    // clear the input so the user can continue adding new countries
+    countryInputEl.value = '';
     let row = document.createElement('tr');
     row.classList.add('mdc-data-table__row');
+    const formDiv = document.createElement('div');
+    formDiv.classList.add('mdc-form-field');
     let countryNameCell = document.createElement('td');
     countryNameCell.classList.add('mdc-data-table__cell');
     let confirmedCell = document.createElement('td');
     confirmedCell.classList.add('mdc-data-table__cell');
+    
+    const confirmedRadioDiv = document.createElement('div');
+    confirmedRadioDiv.classList.add('mdc-radio');
+    const confirmedRadioInput = document.createElement('input');
+    confirmedRadioInput.classList.add('mdc-radio__native-control');
+    confirmedRadioInput.setAttribute('type', 'radio');
+    confirmedRadioInput.setAttribute('name', selectedCountry + '-radio');
+    confirmedRadioInput.setAttribute('checked', true);
+    confirmedRadioInput.id = 'confirmed-radio';
+    const confirmedRadioBgDiv = document.createElement('div');
+    confirmedRadioBgDiv.classList.add('mdc-radio__background');
+    const confirmedRadioOuterCirleDiv = document.createElement('div');
+    confirmedRadioOuterCirleDiv.classList.add('mdc-radio__outer-circle');
+    const confirmedRadioInnerCirleDiv = document.createElement('div');
+    confirmedRadioInnerCirleDiv.classList.add('mdc-radio__inner-circle');
+    const confirmedRadioRippleDiv = document.createElement('div');
+    confirmedRadioRippleDiv.classList.add('mdc-radio__ripple');
+    confirmedRadioBgDiv.appendChild(confirmedRadioOuterCirleDiv);
+    confirmedRadioBgDiv.appendChild(confirmedRadioInnerCirleDiv);
+    confirmedRadioDiv.appendChild(confirmedRadioInput);
+    confirmedRadioDiv.appendChild(confirmedRadioBgDiv);
+    confirmedRadioDiv.appendChild(confirmedRadioRippleDiv);
+    formDiv.appendChild(confirmedRadioDiv);
+    
     let deathsCell = document.createElement('td');
     deathsCell.classList.add('mdc-data-table__cell');
+    
+    const deathsRadioDiv = document.createElement('div');
+    deathsRadioDiv.classList.add('mdc-radio');
+    const deathsRadioInput = document.createElement('input');
+    deathsRadioInput.classList.add('mdc-radio__native-control');
+    deathsRadioInput.setAttribute('type', 'radio');
+    deathsRadioInput.setAttribute('name', selectedCountry + '-radio');
+    deathsRadioInput.id = 'deaths-radio';
+    const deathsRadioBgDiv = document.createElement('div');
+    deathsRadioBgDiv.classList.add('mdc-radio__background');
+    const deathsRadioOuterCirleDiv = document.createElement('div');
+    deathsRadioOuterCirleDiv.classList.add('mdc-radio__outer-circle');
+    const deathsRadioInnerCirleDiv = document.createElement('div');
+    deathsRadioInnerCirleDiv.classList.add('mdc-radio__inner-circle');
+    const deathsRadioRippleDiv = document.createElement('div');
+    deathsRadioRippleDiv.classList.add('mdc-radio__ripple');
+    deathsRadioBgDiv.appendChild(deathsRadioOuterCirleDiv);
+    deathsRadioBgDiv.appendChild(deathsRadioInnerCirleDiv);
+    deathsRadioDiv.appendChild(deathsRadioInput);
+    deathsRadioDiv.appendChild(deathsRadioBgDiv);
+    deathsRadioDiv.appendChild(deathsRadioRippleDiv);
+    formDiv.appendChild(deathsRadioDiv);
+    
     let recoveredCell = document.createElement('td');
     recoveredCell.classList.add('mdc-data-table__cell');
+    
+    const recoveredRadioDiv = document.createElement('div');
+    recoveredRadioDiv.classList.add('mdc-radio');
+    const recoveredRadioInput = document.createElement('input');
+    recoveredRadioInput.classList.add('mdc-radio__native-control');
+    recoveredRadioInput.setAttribute('type', 'radio');
+    recoveredRadioInput.setAttribute('name', selectedCountry + '-radio');
+    recoveredRadioInput.id = 'recovered-radio';
+    const recoveredRadioBgDiv = document.createElement('div');
+    recoveredRadioBgDiv.classList.add('mdc-radio__background');
+    const recoveredRadioOuterCirleDiv = document.createElement('div');
+    recoveredRadioOuterCirleDiv.classList.add('mdc-radio__outer-circle');
+    const recoveredRadioInnerCirleDiv = document.createElement('div');
+    recoveredRadioInnerCirleDiv.classList.add('mdc-radio__inner-circle');
+    const recoveredRadioRippleDiv = document.createElement('div');
+    recoveredRadioRippleDiv.classList.add('mdc-radio__ripple');
+    recoveredRadioBgDiv.appendChild(recoveredRadioOuterCirleDiv);
+    recoveredRadioBgDiv.appendChild(recoveredRadioInnerCirleDiv);
+    recoveredRadioDiv.appendChild(recoveredRadioInput);
+    recoveredRadioDiv.appendChild(recoveredRadioBgDiv);
+    recoveredRadioDiv.appendChild(recoveredRadioRippleDiv);
+    formDiv.appendChild(recoveredRadioDiv);
+    formDiv.id = `${selectedCountry}-form`;
+    
     countryNameCell.textContent = selectedCountry;
-    confirmedCell.textContent = "?";
-    deathsCell.textContent = "?";
-    recoveredCell.textContent = "?";
     row.appendChild(countryNameCell);
     row.appendChild(confirmedCell);
     row.appendChild(deathsCell);
     row.appendChild(recoveredCell);
+    
+    
+    
+    
+    confirmedCell.appendChild(confirmedRadioDiv);
+    deathsCell.appendChild(deathsRadioDiv);
+    recoveredCell.appendChild(recoveredRadioDiv);
     selectedCountriesListTableBody.appendChild(row);
+    
 }
+
+//  <div class="mdc-form-field">
+//   <div class="mdc-radio">
+//     <input class="mdc-radio__native-control" type="radio" id="radio-1" name="radios" checked>
+//     <div class="mdc-radio__background">
+//       <div class="mdc-radio__outer-circle"></div>
+//       <div class="mdc-radio__inner-circle"></div>
+//     </div>
+//     <div class="mdc-radio__ripple"></div>
+//   </div>
+//  <div class="mdc-radio">
+//     <input class="mdc-radio__native-control" type="radio" id="radio-2" name="radios">
+//     <div class="mdc-radio__background">
+//       <div class="mdc-radio__outer-circle"></div>
+//       <div class="mdc-radio__inner-circle"></div>
+//     </div>
+//     <div class="mdc-radio__ripple"></div>
+//   </div>
+//  <div class="mdc-radio">
+//     <input class="mdc-radio__native-control" type="radio" id="radio-3" name="radios">
+//     <div class="mdc-radio__background">
+//       <div class="mdc-radio__outer-circle"></div>
+//       <div class="mdc-radio__inner-circle"></div>
+//     </div>
+//     <div class="mdc-radio__ripple"></div>
+//   </div>
+// </div>
+      
 topBarLinks.forEach((item) => {
     item.addEventListener('click', () => {
         hideViews();
         let target = item.getAttribute("href");
         document.querySelector(target).style.display = "block";
     });
-})
+});
