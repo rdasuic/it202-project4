32e52f70fe4d642cf27c040ca28b31f2c6505a41
Rakesh Das <rdas7@uic.edu>
2020-04-17 02:21:27 +0000

    Removed extraneous code

--- js/script.js
+++ js/script.js
@@ -4,9 +4,6 @@ const searchView = document.querySelector('#search-view');
 const listView = document.querySelector('#list-view');
 const chartView = document.querySelector('#chart-view');
 const topBarLinks = document.querySelectorAll('.mdc-top-app-bar__action-item');
-const confirmedCheckboxEl = document.querySelector('#confirmed-checkbox');
-const deathsCheckboxEl = document.querySelector('#deaths-checkbox');
-const recoveredCheckboxEl = document.querySelector('#recovered-checkbox');
 const countryInputEl = document.querySelector('#country-input');
 const errorDialogEl = document.querySelector('#mdc-dialog-chart-error');
 const errorDialog = new mdc.dialog.MDCDialog(errorDialogEl);
@@ -17,9 +14,6 @@ const noDataErrEls = document.querySelectorAll('.no-data-error');
 const selectedCountriesListEl = document.querySelector('.selected-countries-list');
 mdc.ripple.MDCRipple.attachTo(viewChartBtnEl);
 mdc.ripple.MDCRipple.attachTo(addCountryBtnEl);
-const confirmedCheckbox = new mdc.checkbox.MDCCheckbox(confirmedCheckboxEl);
-const recoveredCheckbox = new mdc.checkbox.MDCCheckbox(recoveredCheckboxEl);
-const deathsCheckbox = new mdc.checkbox.MDCCheckbox(deathsCheckboxEl);
 let allData = {}; // global var for all of the data loaded from the api
 // load the data from the api
 fetch("https://pomber.github.io/covid19/timeseries.json")
@@ -36,55 +30,48 @@ fetch("https://pomber.github.io/covid19/timeseries.json")
 });
 
 viewChartBtnEl.addEventListener('click', () => {
-    if(countryInputEl.value == '' || (!confirmedCheckbox.checked && !deathsCheckbox.checked && !recoveredCheckbox.checked)) {
-        errorDialog.open();
-    }
-    else {
-        const countryData = allData[countryInputEl.value];
-        generateDataTable(countryData);
-        const chartLabels = countryData.map((data) => data.date); // grabs all the dates from each obj
-        const chartDataConfirmed = countryData.map((data) => data.confirmed);
-        const chartDataRecovered = countryData.map((data) => data.recovered);
-        const chartDataDeaths = countryData.map((data) => data.deaths);
-        const chartConfirmedDataset = {
-                    label: 'Confirmed',
-                    data: chartDataConfirmed,
-                    fill: false,
-                    borderColor: 'orange',
-                    pointBackgroundColor: 'orange'
-                }
-        const chartRecoveredDataset = {
-                    label: 'Recovered',
-                    data: chartDataRecovered,
-                    fill: false,
-                    borderColor: 'green',
-                    pointBackgroundColor: 'green'
-                }
-        const chartDeathsDataset = {
-                    label: 'Deaths',
-                    data: chartDataDeaths,
-                    fill: false,
-                    borderColor: 'red',
-                    pointBackgroundColor: 'red'
-                }
-        const chartConfig = {
-            type: 'line',
-            data: {
-                labels: chartLabels,
-                datasets: []
-            },
-            options: {
-                responsive: true
+    const countryData = allData[countryInputEl.value];
+    generateDataTable(countryData);
+    const chartLabels = countryData.map((data) => data.date); // grabs all the dates from each obj
+    const chartDataConfirmed = countryData.map((data) => data.confirmed);
+    const chartDataRecovered = countryData.map((data) => data.recovered);
+    const chartDataDeaths = countryData.map((data) => data.deaths);
+    const chartConfirmedDataset = {
+                label: 'Confirmed',
+                data: chartDataConfirmed,
+                fill: false,
+                borderColor: 'orange',
+                pointBackgroundColor: 'orange'
             }
+    const chartRecoveredDataset = {
+                label: 'Recovered',
+                data: chartDataRecovered,
+                fill: false,
+                borderColor: 'green',
+                pointBackgroundColor: 'green'
+            }
+    const chartDeathsDataset = {
+                label: 'Deaths',
+                data: chartDataDeaths,
+                fill: false,
+                borderColor: 'red',
+                pointBackgroundColor: 'red'
+            }
+    const chartConfig = {
+        type: 'line',
+        data: {
+            labels: chartLabels,
+            datasets: []
+        },
+        options: {
+            responsive: true
         }
-        if(confirmedCheckbox.checked) chartConfig.data.datasets.push(chartConfirmedDataset);
-        if (recoveredCheckbox.checked) chartConfig.data.datasets.push(chartRecoveredDataset);
-        if (deathsCheckbox.checked) chartConfig.data.datasets.push(chartDeathsDataset);
-        new Chart(coronaChartCanvas, chartConfig);
-        hideNoDataErrs();
-        searchView.style.display = "none";
-        chartView.style.display = "block";    
     }
+    new Chart(coronaChartCanvas, chartConfig);
+    hideNoDataErrs();
+    searchView.style.display = "none";
+    chartView.style.display = "block";    
+    
 });
 
 addCountryBtnEl.addEventListener('click', () =>{
@@ -239,33 +226,6 @@ const addCountryToList = () => {
     selectedCountriesListTableBody.appendChild(row);
     
 }
-
-//  <div class="mdc-form-field">
-//   <div class="mdc-radio">
-//     <input class="mdc-radio__native-control" type="radio" id="radio-1" name="radios" checked>
-//     <div class="mdc-radio__background">
-//       <div class="mdc-radio__outer-circle"></div>
-//       <div class="mdc-radio__inner-circle"></div>
-//     </div>
-//     <div class="mdc-radio__ripple"></div>
-//   </div>
-//  <div class="mdc-radio">
-//     <input class="mdc-radio__native-control" type="radio" id="radio-2" name="radios">
-//     <div class="mdc-radio__background">
-//       <div class="mdc-radio__outer-circle"></div>
-//       <div class="mdc-radio__inner-circle"></div>
-//     </div>
-//     <div class="mdc-radio__ripple"></div>
-//   </div>
-//  <div class="mdc-radio">
-//     <input class="mdc-radio__native-control" type="radio" id="radio-3" name="radios">
-//     <div class="mdc-radio__background">
-//       <div class="mdc-radio__outer-circle"></div>
-//       <div class="mdc-radio__inner-circle"></div>
-//     </div>
-//     <div class="mdc-radio__ripple"></div>
-//   </div>
-// </div>
       
 topBarLinks.forEach((item) => {
     item.addEventListener('click', () => {
